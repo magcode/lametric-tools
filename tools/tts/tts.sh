03c451b84a6b8483378c4ab54c5d3308f8239c17
magcode <marko.web1@outlook.com>
2018-08-27 14:56:16 +0200

    proxy support and language configurable

--- tools/tts/tts.sh
+++ tools/tts/tts.sh
@@ -5,6 +5,9 @@ TMPDIR=/mnt/user/temp/tts
 TTS_PATH=/mnt/user/custom/tts
 MP3PLAY_PATH=/mnt/user/custom/playmp3
 
+source $TTS_PATH/tts.conf
+export HTTP_PROXY=$HTTP_PROXY
+export HTTPS_PROXY=$HTTPS_PROXY
 export PYTHONPATH=/mnt/user/custom/tts/pythonlib
 
 mkdir -p $TMPDIR
@@ -13,7 +16,7 @@ mp3file=$(echo $ARG | sed -e 's/[^A-Za-z0-9._-]/_/g')
 mp3file=$TMPDIR/$mp3file.mp3
 
 if [ ! -f $mp3file ]; then
-  $($TTS_PATH/createmp3.sh "$ARG" --output $mp3file --lang de)
+  $($TTS_PATH/createmp3.sh "$ARG" --output $mp3file --lang $lang)
 fi
 
 $($MP3PLAY_PATH/play.sh $mp3file)
