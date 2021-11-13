8c5b095aa5090cdafec55c0f14776f5909a54b5d
magcode <marko.web1@outlook.com>
2021-11-13 16:46:19 +0100

    Update build-gtts-folder.sh

--- build-gtts-folder.sh
+++ build-gtts-folder.sh
@@ -4,5 +4,5 @@ sudo pip install virtualenv
 virtualenv -p /usr/bin/python2.7 ~/virtual
 source ~/virtual/bin/activate
 pip install gtts
-scp -r virtual/lib/python2.7/site-packages lametric:/mnt/user/custom/tts/pythonlib
+scp -r virtual/lib/python2.7/site-packages root@lametric:/mnt/user/custom/tts/pythonlib
 rm -rf ~/virtual
